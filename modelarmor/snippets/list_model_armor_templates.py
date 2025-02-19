4f1410903facbdb9cbff4f79b56decd5a6880a39
vrajpatelll30 <vraj.patel2@crestdata.ai>
2025-02-20 02:22:24 +0530

    Added global snippets

---
+++ modelarmor/snippets/list_model_armor_templates.py
@@ -0,0 +1,63 @@
+# python-docs-samples/modelarmor/list_model_armor_templates.py
+
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import argparse
+from typing import List
+
+# [START list_model_armor_templates]
+def list_model_armor_templates(project_id: str, location_id: str) -> List[str]:
+    """
+    Lists all model armor templates in the specified project and location.
+
+    Args:
+        project_id (str): Google Cloud project ID.
+        location_id (str): Google Cloud location.
+
+    Returns:
+        List[str]: A list of template names.
+    """
+    from google.cloud import modelarmor_v1
+    from google.api_core.client_options import ClientOptions
+
+    # Create Model Armor Client
+    client = modelarmor_v1.ModelArmorClient()
+
+    # Preparing the parent path
+    parent = f"projects/{project_id}/locations/{location_id}"
+
+    # List templates request
+    templates = client.list_templates(parent=parent)
+    
+    # Print templates name only
+    templates_name = [template.name for template in templates]
+    print(f"Templates Found: {', '.join(template_name for template_name in templates_name)}")
+    # [END list_model_armor_templates]
+    
+    return templates
+
+if __name__ == "__main__":
+    # Sample usage
+    parser = argparse.ArgumentParser(
+        description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+
+    parser.add_argument("project_id", help="Google Cloud project ID")
+    parser.add_argument("location_id", help="Google Cloud location")
+
+    args = parser.parse_args()
+
+    # Call the function to list templates
+    list_model_armor_templates(args.project_id, args.location_id)
---
+++ modelarmor/snippets/view_model_armor_template.py
@@ -0,0 +1,63 @@
+# python-docs-samples/modelarmor/view_model_armor_template.py
+
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import argparse
+from typing import Dict
+
+# [START view_modelarmor_template]
+def view_model_armor_template(project_id: str, location_id: str, template_id: str) -> Dict:
+    """
+    Retrieves details for a model armor template.
+
+    Args:
+        project_id (str): Google Cloud project ID where the template exists.
+        location_id (str): Google Cloud location where the template exists.
+        template_id (str): ID of the template to view.
+
+    Returns:
+        Dict: A dictionary containing the details of the Model Armor Template.
+    """
+    from google.cloud import modelarmor_v1
+    from google.api_core.client_options import ClientOptions
+
+    # Create Model Armor client.
+    client = modelarmor_v1.ModelArmorClient()
+
+    # Build the fully qualified name of the template.
+    template_name = f"projects/{project_id}/locations/{location_id}/templates/{template_id}"
+
+    # Get the template details using the client.
+    template = client.get_template(name=template_name)
+    
+    # Print the template name and details.
+    print(f"Model Armor Template Details: {template}")
+    # [END view_modelarmor_template]
+    return template
+
+if __name__ == "__main__":
+    # Sample usage
+    # parser = argparse.ArgumentParser(
+    #     description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter
+    # )
+    # parser.add_argument("project_id", help="The project ID where the template exists.")
+    # parser.add_argument("location_id", help="The location where the template exists.")
+    # parser.add_argument("template_id", help="The ID of the template to view.")
+    
+    # args = parser.parse_args()
+
+    # Call the function to view template details
+    # view_model_armor_template(args.project_id, args.location_id, args.template_id)
+    view_model_armor_template("ma-crest-data-test", "us-central1", "test-template-with-metadata2")
